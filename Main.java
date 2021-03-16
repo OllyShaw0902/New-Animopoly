ddab44621620a7f6863d3e8bc7f385fe11a18378
elliottAbbott <80411029+elliottAbbott@users.noreply.github.com>
2021-03-16 17:21:13 +0000

    Create Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 package com.company;
-import java.util.*;
+import java.util.Random;
 
 public class Main {
 
@@ -69,7 +69,51 @@ public class Main {
         
         //board setup
         Board();
+        
+        //Rolls the dice
+        ArrayList<Integer> diceRolls = new ArrayList<Integer>();
+        diceRolls = Dice();
+
+        if(diceRolls.get(0) == 1){
+            Cards();
+        }
     }
+    
+    //Dice method
+    public static ArrayList<Integer> Dice(){
+        Scanner reader = new Scanner(System.in);
+        Random rand = new Random();
+
+        int dice1 = 0;
+        int dice2 = 0;
+        int doub = 0;
+
+        System.out.println("Press enter to roll the dice.... ");
+        String blank = reader.nextLine();
+
+        System.out.println("Rolling Dice.....");
+
+        dice1 = rand.nextInt(5) + 1;
+        dice2 = rand.nextInt(5) + 1;
+
+        int total = dice1 + dice2;
+
+        System.out.println("You got "+dice1+" and "+dice2);
+        System.out.println("In total you have rolled "+total);
+
+        if(dice1 == dice2){
+            System.out.println("Congratulations, you roll a double, pick up a card!");
+            doub = 1;
+        }
+
+        ArrayList<Integer> dice = new ArrayList<Integer>();
+        dice.add(doub);
+        dice.add(total);
+
+
+        return dice;
+    }
+    
     public static void Board(){
 
         //Currently prints uneditable board 
