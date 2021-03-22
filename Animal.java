294c6b57788f616f5e68d0232e9521dbf2ec6bff
elliottAbbott <80411029+elliottAbbott@users.noreply.github.com>
2021-03-22 11:03:07 +0000

    Changed the Occupied by array and moved the adds inside the method so that the syntax is correct

--- Animal.java
+++ Animal.java
@@ -1,23 +1,27 @@
 package com.company;
-
+import java.util.ArrayList;
 public class Animal {
     private String name;
     private int level=1, cost=0, visitCost=0, purchased=0, tileNum=0;
 
+    public ArrayList<String> occupiedBy = new ArrayList<>();
+
     //for setup
-    public int Animal(String inputName,int inputLevel, int inputCost, int visitCost, int inputPurchased, int inputTileNum) {
+    public Animal(String inputName,int inputLevel, int inputCost, int visitCost, int inputPurchased, int inputTileNum) {
         name=inputName;
         level=inputLevel;
         cost=inputCost;
         purchased=inputPurchased;
         tileNum=inputTileNum;
+
+        occupiedBy.add("*");
+        occupiedBy.add("!");
+        occupiedBy.add("?");
+        occupiedBy.add("%");
     }
 
-    public ArrayList<occupiedBy> = new ArrayList<>();
-       occupiedBy.add("*");
-       occupiedBy.add("!");
-       occupiedBy.add("?");
-       occupiedBy.add("%");
+
+
 
     //for printing to board
     public int getLevel() {
